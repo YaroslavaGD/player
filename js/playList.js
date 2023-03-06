8b348dc6c541b3e8c353cf732446fe131b80dd53
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2023-03-06 22:39:05 +0100

    feat: connecting the main files to the project

---
+++ js/playList.js
@@ -0,0 +1,28 @@
+const playList = [
+  {
+    title: 'Boots! Boots! Boots!',
+    src: 'assets/sounds/test/BootsBootsBoots.mp3',
+    duration: '01:58'
+  },
+  {
+    title: 'Broke For Free - Night Owl',
+    src: 'assets/sounds/test/BrokeForFree-NightOwl.mp3',
+    duration: '03:14'
+  },
+  {
+    title: 'Audionautix - Brookers Blues',
+    src: 'assets/sounds/test/BrookersBlues.mp3',
+    duration: '03:14'
+  },
+  {
+    title: 'Plushgoolash - Big Gay Water Fight',
+    src: 'assets/sounds/test/Plushgoolash-BigGayWaterFight.mp3',
+    duration: '02:35'
+  },
+  {
+    title: 'Solo Acoustic Blues',
+    src: 'assets/sounds/test/SoloAcousticBlues.mp3',
+    duration: '03:00'
+  }
+]
+export default playList;
